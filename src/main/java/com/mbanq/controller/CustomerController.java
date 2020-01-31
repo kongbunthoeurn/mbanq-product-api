b7189533f932e46772896591ae5fa38339579b9d
Kong Bunthoeurn <41603498+kongbunthoeurn@users.noreply.github.com>
2020-01-31 11:55:41 +0700

    update api
    update api

--- src/main/java/com/mbanq/controller/CustomerController.java
+++ src/main/java/com/mbanq/controller/CustomerController.java
@@ -1,5 +1,10 @@
 package com.mbanq.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/admin/api")
 public class CustomerController {
-	@GetMapping("/list-customer")
+	
 	public String showHome() {
 		return "hello";
+	}
+	@GetMapping("/list-customer")
+	ResponseEntity<Map<String, String>> list_customer(){
+		Map<String, String> maps=new HashMap<String, String>();
+		maps.put("S1", "Hello");
+		maps.put("S2", "Hello");
+		return new ResponseEntity<Map<String,String>>(maps, HttpStatus.OK);
 	}
 }
